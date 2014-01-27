264888ecd9494debb5ed58861d370c1437d93490
Jacopo Santoni <jack@pixbits.com>
2014-01-27 20:08:58 +0100

    improved gcwinterface

--- gcwinterface.cpp
+++ gcwinterface.cpp
@@ -5,15 +5,24 @@ extern "C" int SDL_main(int argc, char** argv);
 
 using namespace std;
 
-class OhBoyEmulator : public EmuInterface
+class OhBoyEmulator : public CoreInterface
 {
+	private:
+
 	public:
+		OhBoyEmulator()
+		{
+      registerInformations(CONSOLE_GAME_BOY, "ohboy", "ohboy", "1.0");
+      registerExtension("gb");
+			registerExtension("gbc");
+		}
+
 		virtual void run(int argc, char **argv) { SDL_main(argc, argv); }
 };
 
 static OhBoyEmulator emulator;
 
-extern "C" EmuInterface* retrieve()
+extern "C" CoreInterface* retrieve()
 {
 	return &emulator;
 }
